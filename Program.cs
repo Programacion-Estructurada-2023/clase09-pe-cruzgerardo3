de34f6349ddb07781b5a498510c45d04e82adde3
Cruz Gerardo <1992.cruzgerardo@gmail.com>
2023-08-27 10:13:05 -0600

    practica clase 9

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+           
+            Console.WriteLine("\n---Validador de Notas ---\n");
+
+         Console.WriteLine("Ingrese la nota obtenida:");
+            double nota = Convert.ToDouble(Console.ReadLine());
+
+            if (nota >= 9.5)
+            {
+                Console.WriteLine("La calificación obtenida es Excelente");
+            }
+            else if (nota >= 8.5)
+            {
+                Console.WriteLine("La calificación obtenida es Muy Buena");
+            }
+            else if (nota >= 7.0)
+            {
+                Console.WriteLine("La calificación obtenida es Buena");
+            }
+            else
+            {
+                Console.WriteLine("La calificación obtenida es Deficiente");
+            }           
+            Console.ReadKey();
+        }
+    }
+}
